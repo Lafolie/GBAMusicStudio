a9af65ebc1f0e12b5e95933ed407ed59c5cf8aaf
Lafolie <longshaw.dale+bitbucket@gmail.com>
2018-08-19 01:16:21 +0100

    Tidied up TrackEditor a little so I can actually read it

--- GBAMusicStudio/UI/TrackEditor.cs
+++ GBAMusicStudio/UI/TrackEditor.cs
@@ -1,5 +1,4 @@
-﻿using BrightIdeasSoftware;
-using GBAMusicStudio.Core;
+﻿using GBAMusicStudio.Core;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -15,22 +14,30 @@ namespace GBAMusicStudio.UI
         int currentTrack = 0;
         List<SongEvent> events;
 
-        readonly ObjectListView listView;
+        readonly ObjectListView trackEventlistView;
         readonly ThemedLabel[] labels = new ThemedLabel[3];
         readonly ThemedNumeric[] args = new ThemedNumeric[3];
 
         readonly ComboBox tracksBox, commandsBox;
-        readonly ThemedButton tvButton, taeButton, treButton;
-        readonly ThemedNumeric[] tvArgs = new ThemedNumeric[2];
+        readonly ThemedButton changeVoicesButton, addEventButton, removeEventButton;
+        readonly ThemedNumeric[] trackVoiceDetails = new ThemedNumeric[2]; //repurposed [0] ("from") to show original value
 
-        readonly ComboBox remapsBox;
-        readonly ThemedButton rfButton, rtButton, gvButton;
+        readonly ComboBox globalRemapsBox;
+        readonly ThemedButton globalRemapFromButton, remapToButton, globalChangeVoicesButton;
         readonly ThemedNumeric[] gvArgs = new ThemedNumeric[2];
 
         internal TrackEditor()
         {
-            int w = 300 - 12 - 6, h = 400 - 24;
-            listView = new ObjectListView
+            //configure window
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            Text = "GBA Music Studio ― Track Editor";
+            ClientSize = new Size(600, 400);
+
+            int paneWidth = 300 - 12 - 6, panelHeight = 400 - 24;
+
+            //setup component panels
+            trackEventlistView = new ObjectListView
             {
                 FullRowSelect = true,
                 HeaderStyle = ColumnHeaderStyle.Nonclickable,
@@ -39,10 +46,11 @@ namespace GBAMusicStudio.UI
                 MultiSelect = false,
                 RowFormatter = RowFormatter,
                 ShowGroups = false,
-                Size = new Size(w, h),
+                Size = new Size(paneWidth, panelHeight),
                 UseFiltering = true,
                 UseFilterIndicator = true
             };
+
             OLVColumn c1, c2, c3, c4;
             c1 = new OLVColumn("Event", "Command.Name");
             c2 = new OLVColumn("Arguments", "Command.Arguments") { UseFiltering = false };
@@ -52,15 +60,15 @@ namespace GBAMusicStudio.UI
             c4.Width = 45;
             c1.Hideable = c2.Hideable = c3.Hideable = c4.Hideable = false;
             c1.TextAlign = c2.TextAlign = c3.TextAlign = c4.TextAlign = HorizontalAlignment.Center;
-            listView.AllColumns.AddRange(new OLVColumn[] { c1, c2, c3, c4 });
-            listView.RebuildColumns();
-            listView.SelectedIndexChanged += SelectedIndexChanged;
-            listView.ItemActivate += ListView_ItemActivate;
+            trackEventlistView.AllColumns.AddRange(new OLVColumn[] { c1, c2, c3, c4 });
+            trackEventlistView.RebuildColumns();
+            trackEventlistView.SelectedIndexChanged += SelectedIndexChanged;
+            trackEventlistView.ItemActivate += ListView_ItemActivate;
 
-            int h2 = h / 3 - 4;
-            var panel1 = new ThemedPanel { Location = new Point(306, 12), Size = new Size(w, h2) };
-            var panel2 = new ThemedPanel { Location = new Point(306, 140), Size = new Size(w, h2 - 1) };
-            var panel3 = new ThemedPanel { Location = new Point(306, 267), Size = new Size(w, h2) };
+            int h2 = panelHeight / 3 - 4;
+            var eventEditPanel = new ThemedPanel { Location = new Point(306, 12), Size = new Size(paneWidth, h2) };
+            var trackEditPanel = new ThemedPanel { Location = new Point(306, 140), Size = new Size(paneWidth, h2 - 1) };
+            var globalEditPanel = new ThemedPanel { Location = new Point(306, 267), Size = new Size(paneWidth, h2) };
 
             // Arguments numericals
             for (int i = 0; i < 3; i++)
@@ -73,16 +81,17 @@ namespace GBAMusicStudio.UI
                     Text = "Arg. " + (i + 1).ToString(),
                     Visible = false,
                 };
+
                 args[i] = new ThemedNumeric
                 {
-                    Location = new Point(w - 152, y),
+                    Location = new Point(paneWidth - 152, y),
                     Maximum = int.MaxValue,
                     Minimum = int.MinValue,
                     Size = new Size(100, 25),
                     Visible = false
                 };
                 args[i].ValueChanged += ArgumentChanged;
-                panel1.Controls.AddRange(new Control[] { labels[i], args[i] });
+                eventEditPanel.Controls.AddRange(new Control[] { labels[i], args[i] });
             }
 
             // Track controls
@@ -92,67 +101,78 @@ namespace GBAMusicStudio.UI
                 Size = new Size(100, 21)
             };
             tracksBox.SelectedIndexChanged += TracksBox_SelectedIndexChanged;
-            tvButton = new ThemedButton
+
+            changeVoicesButton = new ThemedButton
             {
                 Location = new Point(13, 30),
                 Text = "Change Voices"
             };
-            tvButton.Click += ChangeEvents;
-            taeButton = new ThemedButton
+            changeVoicesButton.Click += ChangeEvents;
+
+            addEventButton = new ThemedButton
             {
                 Location = new Point(13, 30 + 25 + 5),
                 Text = "Add Event"
             };
-            taeButton.Click += AddEvent;
+            addEventButton.Click += AddEvent;
+
             commandsBox = new ComboBox
             {
                 Location = new Point(115, 30 + 25 + 5 + 2),
                 Size = new Size(100, 21)
             };
-            treButton = new ThemedButton
+            removeEventButton = new ThemedButton
             {
                 Location = new Point(13, 30 + 25 + 5 + 25 + 5),
                 Text = "Remove Event"
             };
-            treButton.Click += RemoveEvent;
-            tracksBox.Enabled = tvButton.Enabled = taeButton.Enabled = treButton.Enabled = commandsBox.Enabled = false;
-            tvButton.Size = taeButton.Size = treButton.Size = new Size(95, 25);
-            var tvFrom = new ThemedLabel { Location = new Point(115, 30 + 2 + 3), Text = "From" };
-            tvArgs[0] = new ThemedNumeric { Location = new Point(149, 30 + 2) };
-            var tvTo = new ThemedLabel { Location = new Point(204, 30 + 2 + 3), Text = "To" };
-            tvArgs[1] = new ThemedNumeric { Location = new Point(224, 30 + 2) };
-            tvArgs[0].Maximum = tvArgs[1].Maximum = 0xFF;
-            tvArgs[0].Size = tvArgs[1].Size = new Size(45, 23);
-            tvArgs[0].TextAlign = tvArgs[1].TextAlign = HorizontalAlignment.Center;
-            tvFrom.AutoSize = tvTo.AutoSize = true;
-            panel2.Controls.AddRange(new Control[] { tracksBox, tvButton, tvFrom, tvTo, tvArgs[0], tvArgs[1], taeButton, commandsBox, treButton });
+            removeEventButton.Click += RemoveEvent;
+
+            //Track controls box preparation
+            tracksBox.Enabled = changeVoicesButton.Enabled = addEventButton.Enabled = removeEventButton.Enabled = commandsBox.Enabled = false;
+            changeVoicesButton.Size = addEventButton.Size = removeEventButton.Size = new Size(95, 25);
+            var originalVoiceLabel = new ThemedLabel { Location = new Point(115, 30 + 2 + 3), Text = "From" };
+            trackVoiceDetails[0] = new ThemedNumeric { Location = new Point(149, 30 + 2) };
+            var newVoiceIDLabel = new ThemedLabel { Location = new Point(204, 30 + 2 + 3), Text = "To" };
+            trackVoiceDetails[1] = new ThemedNumeric { Location = new Point(224, 30 + 2) };
+            trackVoiceDetails[0].Maximum = trackVoiceDetails[1].Maximum = 0xFF;
+            trackVoiceDetails[0].Size = trackVoiceDetails[1].Size = new Size(45, 23);
+            trackVoiceDetails[0].TextAlign = trackVoiceDetails[1].TextAlign = HorizontalAlignment.Center;
+            originalVoiceLabel.AutoSize = newVoiceIDLabel.AutoSize = true;
+
+            trackEditPanel.Controls.AddRange(new Control[] { tracksBox, changeVoicesButton, originalVoiceLabel, newVoiceIDLabel, trackVoiceDetails[0], trackVoiceDetails[1], addEventButton, commandsBox, removeEventButton });
 
             // Global controls
-            remapsBox = new ComboBox
+            globalRemapsBox = new ComboBox
             {
                 DataSource = Config.InstrumentRemaps.Keys.ToArray(),
                 Location = new Point(4, 4),
                 Size = new Size(100, 21)
             };
-            rfButton = new ThemedButton
+
+            globalRemapFromButton = new ThemedButton
             {
                 Location = new Point(116, 3),
                 Text = "From"
             };
-            rfButton.Click += (s, e) => ApplyRemap(true);
-            rtButton = new ThemedButton
+            globalRemapFromButton.Click += (s, e) => ApplyRemap(true);
+
+            remapToButton = new ThemedButton
             {
                 Location = new Point(203, 3),
                 Text = "To"
             };
-            rtButton.Click += (s, e) => ApplyRemap(false);
-            gvButton = new ThemedButton
+            remapToButton.Click += (s, e) => ApplyRemap(false);
+
+            globalChangeVoicesButton = new ThemedButton
             {
                 Location = new Point(13, 30),
                 Size = new Size(95, 25),
                 Text = "Change Voices"
             };
-            gvButton.Click += ChangeAllEvents;
+            globalChangeVoicesButton.Click += ChangeAllEvents;
+
+            //Global remap controls box preparation
             var gvFrom = new ThemedLabel { Location = new Point(115, 30 + 2 + 3), Text = "From" };
             gvArgs[0] = new ThemedNumeric { Location = new Point(149, 30 + 2) };
             var gvTo = new ThemedLabel { Location = new Point(204, 30 + 2 + 3), Text = "To" };
@@ -161,38 +181,37 @@ namespace GBAMusicStudio.UI
             gvArgs[0].Size = gvArgs[1].Size = new Size(45, 23);
             gvArgs[0].TextAlign = gvArgs[1].TextAlign = HorizontalAlignment.Center;
             gvFrom.AutoSize = gvTo.AutoSize = true;
-            remapsBox.Enabled = rfButton.Enabled = rtButton.Enabled = gvButton.Enabled = false;
-            panel3.Controls.AddRange(new Control[] { remapsBox, rfButton, rtButton, gvButton, gvFrom, gvTo, gvArgs[0], gvArgs[1] });
+            globalRemapsBox.Enabled = globalRemapFromButton.Enabled = remapToButton.Enabled = globalChangeVoicesButton.Enabled = false;
 
-            ClientSize = new Size(600, 400);
-            Controls.AddRange(new Control[] { listView, panel1, panel2, panel3 });
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-            MaximizeBox = false;
-            Text = "GBA Music Studio ― Track Editor";
+            globalEditPanel.Controls.AddRange(new Control[] { globalRemapsBox, globalRemapFromButton, remapToButton, globalChangeVoicesButton, gvFrom, gvTo, gvArgs[0], gvArgs[1] });
+
+            //fin
+            Controls.AddRange(new Control[] { trackEventlistView, eventEditPanel, trackEditPanel, globalEditPanel });
 
             UpdateTracks();
         }
 
         void ListView_ItemActivate(object sender, EventArgs e)
         {
-            SongPlayer.SetPosition(((SongEvent)listView.SelectedItem.RowObject).AbsoluteTicks);
+            SongPlayer.SetPosition(((SongEvent)trackEventlistView.SelectedItem.RowObject).AbsoluteTicks);
         }
 
         void AddEvent(object sender, EventArgs e)
         {
             var cmd = (ICommand)Activator.CreateInstance(Engine.GetCommands()[commandsBox.SelectedIndex].GetType());
             var ev = new SongEvent(0xFFFFFFFF, cmd);
-            int index = listView.SelectedIndex + 1;
+            int index = trackEventlistView.SelectedIndex + 1;
             SongPlayer.Song.InsertEvent(ev, currentTrack, index);
             SongPlayer.RefreshSong();
             LoadTrack(currentTrack);
             SelectItem(index);
         }
+
         void RemoveEvent(object sender, EventArgs e)
         {
-            if (listView.SelectedIndex == -1)
+            if (trackEventlistView.SelectedIndex == -1)
                 return;
-            SongPlayer.Song.RemoveEvent(currentTrack, listView.SelectedIndex);
+            SongPlayer.Song.RemoveEvent(currentTrack, trackEventlistView.SelectedIndex);
             SongPlayer.RefreshSong();
             LoadTrack(currentTrack);
         }
@@ -223,7 +242,7 @@ namespace GBAMusicStudio.UI
         void ApplyRemap(bool from)
         {
             bool changed = false;
-            string remap = (string)remapsBox.SelectedItem;
+            string remap = (string)globalRemapsBox.SelectedItem;
             foreach (var track in SongPlayer.Song.Commands)
                 foreach (var ev in track)
                     if (ev.Command is VoiceCommand voice)
@@ -237,13 +256,14 @@ namespace GBAMusicStudio.UI
                 LoadTrack(currentTrack);
             }
         }
+
         void ChangeEvents(object sender, EventArgs e)
         {
             bool changed = false;
             foreach (var ev in events)
-                if (sender == tvButton && ev.Command is VoiceCommand voice && voice.Voice == tvArgs[0].Value)
+                if (sender == changeVoicesButton && ev.Command is VoiceCommand voice && voice.Voice == trackVoiceDetails[0].Value)
                 {
-                    voice.Voice = (byte)tvArgs[1].Value;
+                    voice.Voice = (byte)trackVoiceDetails[1].Value;
                     changed = true;
                 }
             if (changed)
@@ -252,12 +272,13 @@ namespace GBAMusicStudio.UI
                 LoadTrack(currentTrack);
             }
         }
+
         void ChangeAllEvents(object sender, EventArgs e)
         {
             bool changed = false;
             foreach (var track in SongPlayer.Song.Commands)
                 foreach (var ev in track)
-                    if (sender == gvButton && ev.Command is VoiceCommand voice && voice.Voice == gvArgs[0].Value)
+                    if (sender == globalChangeVoicesButton && ev.Command is VoiceCommand voice && voice.Voice == gvArgs[0].Value)
                     {
                         voice.Voice = (byte)gvArgs[1].Value;
                         changed = true;
@@ -273,28 +294,30 @@ namespace GBAMusicStudio.UI
         {
             currentTrack = track;
             events = SongPlayer.Song.Commands[track];
-            listView.SetObjects(events);
+            trackEventlistView.SetObjects(events);
             SelectedIndexChanged(null, null);
         }
+
         void TracksBox_SelectedIndexChanged(object sender, EventArgs e) => LoadTrack(tracksBox.SelectedIndex);
         internal void UpdateTracks()
         {
             bool tracks = SongPlayer.NumTracks > 0;
-            tracksBox.Enabled = tvButton.Enabled = taeButton.Enabled = treButton.Enabled = commandsBox.Enabled = gvButton.Enabled = tracks;
+            tracksBox.Enabled = changeVoicesButton.Enabled = addEventButton.Enabled = removeEventButton.Enabled = commandsBox.Enabled = globalChangeVoicesButton.Enabled = tracks;
 
             tracksBox.DataSource = Enumerable.Range(1, SongPlayer.NumTracks).Select(i => $"Track {i}").ToList();
-            remapsBox.Enabled = rfButton.Enabled = rtButton.Enabled = tracks && remapsBox.Items.Count > 0;
+            globalRemapsBox.Enabled = globalRemapFromButton.Enabled = remapToButton.Enabled = tracks && globalRemapsBox.Items.Count > 0;
 
             commandsBox.DataSource = Engine.GetCommands().Select(c => c.Name).ToList();
 
             if (!tracks)
-                listView.Items.Clear();
+                trackEventlistView.Items.Clear();
         }
+
         void SelectItem(int index)
         {
-            listView.Items[index].Selected = true;
-            listView.Select();
-            listView.EnsureVisible(index);
+            trackEventlistView.Items[index].Selected = true;
+            trackEventlistView.Select();
+            trackEventlistView.EnsureVisible(index);
         }
 
         void ArgumentChanged(object sender, EventArgs e)
@@ -303,7 +326,7 @@ namespace GBAMusicStudio.UI
             {
                 if (sender == args[i])
                 {
-                    var se = events[listView.SelectedIndices[0]];
+                    var se = events[trackEventlistView.SelectedIndices[0]];
                     object value = args[i].Value;
                     var m = se.Command.GetType().GetMember(labels[i].Text)[0];
                     if (m is FieldInfo f)
@@ -313,7 +336,7 @@ namespace GBAMusicStudio.UI
                     SongPlayer.RefreshSong();
 
                     var control = ActiveControl;
-                    int index = listView.SelectedIndex;
+                    int index = trackEventlistView.SelectedIndex;
                     LoadTrack(currentTrack);
                     SelectItem(index);
                     control.Select();
@@ -322,16 +345,17 @@ namespace GBAMusicStudio.UI
                 }
             }
         }
+
         void SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (listView.SelectedIndices.Count != 1)
+            if (trackEventlistView.SelectedIndices.Count != 1)
             {
                 labels[0].Visible = labels[1].Visible = labels[2].Visible =
                     args[0].Visible = args[1].Visible = args[2].Visible = false;
             }
             else
             {
-                var se = (SongEvent)listView.SelectedObject;
+                var se = (SongEvent)trackEventlistView.SelectedObject;
                 var ignore = typeof(ICommand).GetMembers();
                 var mi = se.Command == null ? new MemberInfo[0] : se.Command.GetType().GetMembers().Where(m => !ignore.Any(a => m.Name == a.Name) && (m is FieldInfo || m is PropertyInfo)).ToArray();
                 for (int i = 0; i < 3; i++)
